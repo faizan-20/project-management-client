2113facd81ce0a0114ff382056c33584d2b1b800
faizan-20 <faizan.20a@gmail.com>
2024-02-03 19:54:54 +0530

    fix: handling refresh token

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import { CreateProjectDialog } from "./CreateProjectDialog";
+import { AuthContext } from "@/context/AuthProvider";
 
 export default function Home() {
+  const { user } = useContext(AuthContext);
+
   return (
     <div>
-      <div>Login Successful</div>
+      <div>Login Successful: {user.firstname}</div>
       <CreateProjectDialog />
     </div>
   );
