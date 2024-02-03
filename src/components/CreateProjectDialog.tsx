6790f5831420952d50acbc6c6d3972314438ac64
faizan-20 <faizan.20a@gmail.com>
2024-02-03 17:49:44 +0530

    add modal and modal layout for 'create project'

---
+++ src/components/CreateProjectDialog.tsx
@@ -0,0 +1,35 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+
+export function CreateProjectDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create Project</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Project</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+
+        <div>Form</div>
+
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
+import { CreateProjectDialog } from "./CreateProjectDialog";
+
 export default function Home() {
   return (
     <div>
       <div>Login Successful</div>
+      <CreateProjectDialog />
     </div>
   );
 }
