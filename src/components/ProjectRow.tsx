52bfe03152766234768221c7bf278215d5847e0d
faizan-20 <faizan.20a@gmail.com>
2024-04-02 14:18:20 +0530

    feat add/remove fav project

--- src/components/ProjectRow.tsx
+++ src/components/ProjectRow.tsx
@@ -7,9 +7,11 @@ import { ProjectsContext } from "@/context/ProjectsProvider";
 
 type ProjectRowProps = {
   project: ProjectType;
+  favProjects: string[];
+  setFavProjects: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
-function ProjectRow({ project }: ProjectRowProps) {
+function ProjectRow({ project, favProjects, setFavProjects }: ProjectRowProps) {
   const { setProjects } = useContext(ProjectsContext);
 
   const deleteProject = async () => {
@@ -23,30 +25,63 @@ function ProjectRow({ project }: ProjectRowProps) {
     }
   };
 
+  const toggleFavoriteProject = async () => {
+    try {
+      if (favProjects.includes(project._id)) {
+        await axiosPrivate.post(
+          `/projects/remove-favour-project/${project._id}`
+        );
+        setFavProjects((prevArray) =>
+          prevArray.filter((item) => item !== project._id)
+        );
+      } else {
+        await axiosPrivate.post(`/projects/add-favour-project/${project._id}`);
+        setFavProjects([...favProjects, project._id]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <tr>
-      <td className="text-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6 text-slate-400 hover:text-slate-900 hover:cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-          />
-        </svg>
+      <td className="text-center" onClick={toggleFavoriteProject}>
+        {favProjects.includes(project._id) ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6 text-amber-400 hover:cursor-pointer"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 text-slate-400 hover:text-slate-900 hover:cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+            />
+          </svg>
+        )}
       </td>
-      <td className="text-blue-800 hover:underline min-w-[6vw] max-w-[6vw] hover:cursor-pointer">
+      <td className="text-blue-800 hover:underline hover:cursor-pointer">
         {project.title}
       </td>
       <td className="min-w-[5vw]">{project.key}</td>
       <td className="max-w-[7vw]">Team-managed Software</td>
-      <td className="text-blue-800 max-w-[6vw]">
+      <td className="text-blue-800">
         <Link to="#" className="hover:underline">
           {project.owner?.firstname}
         </Link>
