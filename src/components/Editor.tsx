dd7af7b8f787daf705bc4b0a522ff32f03980c91
faizan-20 <faizan.20a@gmail.com>
2024-04-15 20:34:56 +0530

    add button to cancel and save

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,9 @@
-import { SetStateAction, useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; // import Quill styles
+import { SetStateAction, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // import Quill styles
 
 const Editor = () => {
-  const [editorHtml, setEditorHtml] = useState('');
+  const [editorHtml, setEditorHtml] = useState("");
 
   const handleChange = (html: SetStateAction<string>) => {
     setEditorHtml(html);
@@ -11,10 +11,10 @@ const Editor = () => {
 
   return (
     <div>
-      <ReactQuill 
+      <ReactQuill
         theme="snow" // specify theme ('snow' or 'bubble')
         value={editorHtml}
-        onChange={handleChange} 
+        onChange={handleChange}
       />
     </div>
   );
