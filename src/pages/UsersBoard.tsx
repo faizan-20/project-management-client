4d7c4fbdff9c235ed60c8a5c1d25eb89717705b2
Ayush <ayushkumar9601@gmail.com>
2024-04-21 23:05:32 +0530

    search functionality complete

--- src/pages/UsersBoard.tsx
+++ src/pages/UsersBoard.tsx
@@ -24,6 +24,8 @@ function UserBoard() {
     const users = currProject?.users;
 
     const [otherUsers, setOtherUsers] = useState<User[]>();
+    const [searchProjectUser, setSearchProjectUser] = useState('');
+    const [searchOtherUser, setSearchOtherUser] = useState('');
 
     const axiosPrivate = useAxiosPrivate();
 
@@ -77,37 +79,19 @@ function UserBoard() {
                 </BreadcrumbList>
             </Breadcrumb>
             <div className="font-semibold py-4 text-2xl">
-                Project Users
+                Users
             </div>
             <div className="flex justify-between">
                 <Input
                     type="text"
                     name="search-issue"
-                    // value={issueSearch}
-                    // onChange={(e) => setIssueSearch(e.target.value)}
+                    value={searchProjectUser}
+                    onChange={(e) => setSearchProjectUser(e.target.value)}
                     id="search-issue"
                     placeholder="Search project users"
                     className="px-2 text-base border-slate-200 border-2 max-w-60"
                 />
 
-                <AddUserSheet projectId={'test-projectid'}>
-                    <div className="inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary disabled:pointer-events-none disabled:opacity-50 py-2 px-4 bg-slate-100 text-secondary-foreground shadow-sm hover:bg-sky-100/80">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-                            />
-                        </svg>
-                    </div>
-                </AddUserSheet>
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8 my-4">
@@ -123,7 +107,16 @@ function UserBoard() {
                 />
 
                 {/* --------------------- ADMIN ------------------- */}
-                {admins?.map((admin) => {
+                {admins?.filter((admin) => {
+                    return searchProjectUser.toLowerCase() === ""
+                        ? admin
+                        : admin.firstname
+                            .toLowerCase()
+                            .includes(searchProjectUser.toLowerCase()) ||
+                        admin.email
+                            .toLowerCase()
+                            .includes(searchProjectUser.toLowerCase())
+                }).map((admin) => {
                     if (owner?._id !== admin._id) {
                         return (
                             <UserCard
@@ -140,7 +133,16 @@ function UserBoard() {
                 })}
 
                 {/* --------------------- USERS ------------------- */}
-                {users?.map((user) => {
+                {users?.filter((user) => {
+                    return searchProjectUser.toLowerCase() === ""
+                        ? user
+                        : user.firstname
+                            .toLowerCase()
+                            .includes(searchProjectUser.toLowerCase()) ||
+                        user.email
+                            .toLowerCase()
+                            .includes(searchProjectUser.toLowerCase())
+                }).map((user) => {
                     return (
                         <UserCard
                             key={user?._id}
@@ -164,8 +166,8 @@ function UserBoard() {
                 <Input
                     type="text"
                     name="search-issue"
-                    // value={issueSearch}
-                    // onChange={(e) => setIssueSearch(e.target.value)}
+                    value={searchOtherUser}
+                    onChange={(e) => setSearchOtherUser(e.target.value)}
                     id="search-issue"
                     placeholder="Search other users"
                     className="px-2 text-base border-slate-200 border-2 max-w-60"
@@ -176,7 +178,16 @@ function UserBoard() {
 
             <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-8 my-4">
                 {/* --------------------- OTHER USERS ------------------- */}
-                {otherUsers?.map((user) => {
+                {otherUsers?.filter((user) => {
+                    return searchOtherUser.toLowerCase() === ""
+                        ? user
+                        : user.firstname
+                            .toLowerCase()
+                            .includes(searchOtherUser.toLowerCase()) ||
+                        user.email
+                            .toLowerCase()
+                            .includes(searchOtherUser.toLowerCase())
+                }).map((user) => {
                     return (
                         <UserCard
                             key={user?._id}
