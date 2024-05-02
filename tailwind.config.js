365da1315cac17d5ba95f7fecd66bcf91aa2a2f6
faizan-20 <faizan.20a@gmail.com>
2024-05-03 00:55:04 +0530

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,8 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        "hero-image": "url(./public/abstract-minimal.jpeg)",
-        "hero-image2": "url(./public/abstract-minimal-2.png)",
+        "hero-image": "url(/abstract-minimal.jpeg)",
+        "hero-image2": "url(/abstract-minimal-2.png)",
       },
       colors: {
         hero: "#ffffff",
